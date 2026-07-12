3b054f6a8227e44f470f32d6067c6576604bca59
jtag <hesamgholami@yahoo.com>
2026-07-12 15:47:02 +0330

    feat: add ability to customize prefix

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
+type PersianPhoneNumberPrefix = "+989" | "0989" | "989" | "09" | "9"
+
 /**
- * Normalize phone number to use this format: +989123456789. By default, this
- * function will convert Persian and Arabic numbers to English counterparts
- * unless you opt out of this behavior using `{forceEnDigits: false}` option.
+ * Normalize any Persian phone number to use this format: +989123456789. The `+98`
+ * prefix is configurable. By default, this function will convert Persian and
+ * Arabic numbers to English counterparts unless you opt out of this behavior using
+ * the `{forceEnDigits: false}` option.
  * In addition to that, this function will validate the phone number to be valid.
  * The validation is not comprehensive, and it only checks for prefix and length.
- * You can opt out of validation by using `{validate: false}` option.
+ * You can opt out of validation by using the `{validate: false}` option.
+ * For changing the default prefix, use the `{prefix: "09"}` option.
  *
- * For example this phone number: `09123456789` will be converted to `+989123456789`.
+ * For example, this phone number: `09123456789` will be converted to `+989123456789`.
  * This one: `912۳۴۵6789` will be converted to `+989123456789`.
  * This one: `123456789` will return false because it doesn't have the correct prefix.
  * This one: `0912345678` will also return false because it doesn't have the correct length.
@@ -14,8 +18,12 @@
  * @param options
  */
 export function normalizeNumber(
-	input: string,
-	options?: { forceEnDigits: boolean; validate: boolean },
+  input: string,
+  options?: {
+    forceEnDigits?: boolean
+    validate?: boolean
+    prefix?: PersianPhoneNumberPrefix
+  },
 ): string | false {
   const enDigits = (() => {
     if (options?.forceEnDigits === false) {
@@ -25,17 +33,17 @@ export function normalizeNumber(
     return faToEnDigits(input)
   })()
 
+  const prefix = options?.prefix ?? "+989"
+
   const normalized = (() => {
-    if (enDigits.startsWith("+98")) {
-      return enDigits
-    } else if (enDigits.startsWith("098")) {
-      return `+${enDigits.substring(1)}`
-    } else if (enDigits.startsWith("98")) {
-      return `+${enDigits}`
+    if (enDigits.startsWith("+989") || enDigits.startsWith("0989")) {
+      return `${prefix}${enDigits.substring(4)}`
+    } else if (enDigits.startsWith("989")) {
+      return `${prefix}${enDigits.substring(3)}`
     } else if (enDigits.startsWith("09")) {
-      return `+98${enDigits.substring(1)}`
+      return `${prefix}${enDigits.substring(2)}`
     } else if (enDigits.startsWith("9")) {
-      return `+98${enDigits}`
+      return `${prefix}${enDigits.substring(1)}`
     }
     return false
   })()
@@ -52,7 +60,12 @@ export function normalizeNumber(
    * At this point we have the normalized number so there is no need to normalize again
    * inside the validator function.
    */
-  if (isValidPersianPhoneNumber(normalized, { normalize: false })) {
+  if (
+    isValidPersianPhoneNumber(normalized, {
+      normalize: false,
+      prefix,
+    })
+  ) {
     return normalized
   }
 
@@ -62,10 +75,10 @@ export function normalizeNumber(
 /**
  * Normalizes a number and check its length and initial characters to match `+989`.
  * Phone number length after internal normalization should be exactly 13 characters
- * including the first + sign. If you want to strict check without normalization,
- * you can pass `{normalize: false}` option to opt out of this behavior.
+ * including the first + sign. If you want to strictly check without normalization,
+ * you can pass the `{normalize: false}` option to opt out of this behavior.
  *
- * For example this number is valid: `0912۳۴۵6789` because after internal normalization
+ * For example, this number is valid: `0912۳۴۵6789` because after internal normalization
  * it'll have the correct length and prefix. But this number is not valid: `+98912345678`
  * because it doesn't have the correct length even after the internal normalization.
  *
@@ -74,20 +87,31 @@ export function normalizeNumber(
  */
 export function isValidPersianPhoneNumber(
   input: string,
-  options?: { normalize: boolean },
+  options?: { normalize?: boolean; prefix?: PersianPhoneNumberPrefix },
 ): boolean {
   const phoneNumber = (() =>
-    options?.normalize === false ? input : normalizeNumber(input))()
+    options?.normalize === false
+      ? input
+      : normalizeNumber(input, { prefix: options?.prefix }))()
+
   if (phoneNumber === false) {
     return false
   }
+
+  const prefix = options?.prefix ?? "+989"
+  const nonNumberStart = prefix.startsWith("+")
+    ? "+"
+    : prefix.startsWith("0")
+      ? "0"
+      : ""
+
   return (
-    phoneNumber.startsWith("+989") &&
-    phoneNumber.length === 13 &&
+    phoneNumber.startsWith(prefix) &&
+    phoneNumber.length === prefix.length + 9 &&
     /**
      * This will make sure something like "123 456" is not passed successfully.
      */
-    `+${parseInt(phoneNumber, 10)}` === phoneNumber
+    `${nonNumberStart}${parseInt(phoneNumber, 10)}` === phoneNumber
   )
 }
 
