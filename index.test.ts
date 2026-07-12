3899fa75eb622cb37a9eb2fe765aefe396f03dfa
jtag <hesamgholami@yahoo.com>
2026-07-12 15:47:25 +0330

    test: add unit tests for prefix

--- index.test.ts
+++ index.test.ts
@@ -60,4 +60,32 @@ test("Should normalize numbers correctly with different options", () => {
   expect(
     normalizeNumber("123۴۵6789", { forceEnDigits: true, validate: true }),
   ).toBeFalse()
+  expect(normalizeNumber("123۴۵6789", { validate: true })).toBeFalse()
+  expect(normalizeNumber("123۴۵6789", { forceEnDigits: true })).toBeFalse()
+  expect(
+    normalizeNumber("912345۶۷", {
+      forceEnDigits: true,
+      validate: false,
+      prefix: "09",
+    }),
+  ).toBe("091234567")
+  expect(
+    normalizeNumber("091234567۸۹", {
+      forceEnDigits: true,
+      validate: true,
+      prefix: "989",
+    }),
+  ).toBe("989123456789")
+})
+
+test("Should apply prefix correctly to number", () => {
+  expect(normalizeNumber("9۱۲۳456789", { prefix: "9" })).toBe("9123456789")
+  expect(normalizeNumber("9125647788", { prefix: "09" })).toBe("09125647788")
+  expect(normalizeNumber("09123456789", { prefix: "989" })).toBe("989123456789")
+  expect(normalizeNumber("91234567۸9", { prefix: "0989" })).toBe(
+    "0989123456789",
+  )
+  expect(normalizeNumber("912۳۴5٦٧89", { prefix: "+989" })).toBe(
+    "+989123456789",
+  )
 })
